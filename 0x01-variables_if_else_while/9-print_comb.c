1c81478bfe7768ea9f4d0844d2c33bb174713d02
D-only-XL <theosamacurtis@gmail.com>
2022-03-12 20:46:54 -0800

    new commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numb;
+
+	for (num = '0' ; numb <= '9 ' ; numb++)
+	{
+		putchar(numb);
+
+		if (numb != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
