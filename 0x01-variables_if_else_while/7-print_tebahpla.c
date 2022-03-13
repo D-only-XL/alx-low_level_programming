630ae4f110e085cb679aef53f7943fc3e48b07ce
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-13 04:42:33 +0100

    new commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main - Entry point
+*main - Entry point
 *
-* Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	char alpha;
-
+char alpha;
+	
 for (alpha = 'z' ; alpha >= 'a' ; alpha--)
 putchar(alpha);
 putchar('\n');
