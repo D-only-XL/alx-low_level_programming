71a9d4e5f0de3e88ec412f874691034a5c087d9a
D-only-XL <theosamacurtis@gmail.com>
2022-03-12 20:26:14 -0800

    new commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numb;
+	char alphab;
+
+	fot (numb = 48 ; numb <= 57 ; numb++)
+		putchar(numb);
+	for (alpha = 97 ; alpha <= 102 ; alpha++)
+		putchar('\n');
+	return (0);
