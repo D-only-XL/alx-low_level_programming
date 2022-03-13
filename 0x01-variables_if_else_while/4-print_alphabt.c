aafdfcd492bf8b2349aad79b249bdeec97a14bc7
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-13 03:26:23 +0100

    new commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,16 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
- char alpha;
+int main(void)
+{
+char alpha;
     
-    for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
            
-      if (alpha != 'q' && alpha != 'e')
-      {
-        putchar(alpha);
-      }
-    putchar('\n');
-    return (0);
-  }
+if (alpha != 'q' && alpha != 'e')
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
