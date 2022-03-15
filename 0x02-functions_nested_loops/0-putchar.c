419d51a6459383715b9fcd1658630b8b023011f1
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-15 16:49:51 +0100

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+* int main(void)
+*{
+Char h[] = "Holberton"
+Int m;
+
+m = 0;
+
+while (m <= 0)
+{
+_putchar(h[m]);
+m++;
+}
+_putchar('\n');
+return (0);
+}
