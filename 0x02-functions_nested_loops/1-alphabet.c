d2464019ff229075b7f50e8bd8dd8939eb6375ec
D-only-XL <theosamacurtis@gmail.com>
2022-03-15 20:28:06 -0700

    new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha) ; alpha++;
+	}
+	_putchar('\n');
+}
