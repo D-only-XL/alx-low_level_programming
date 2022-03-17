bd0aecace15589df35844997df13ba8a12d9ea20
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-17 14:57:52 +0100

    Update main.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
 *main - Test function for positive or negative integers
@@ -9,8 +9,8 @@ int main(void)
 
 {
 int i;
+  
 i = 0;
-
 positive_or_negative(i);
 
 return (0);
