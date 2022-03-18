3375fb5d1dd95af68742c8d4a97ed0cfca52e9f2
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-18 04:22:15 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main,h"
+
+/**
+*_isdigit - function that prints the sign of a number
+*
+*Return: 1 if c is a digit, o otherwise
+*/
+
+int _isdigit(c)
+{
+if (c >= '0' && c <= '9')
+{
+return(1);
+}
+else
+return (0);
+}
