8dbe6b94cc26ea1ca4248aec6ae15ef3c6e57bab
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-18 17:19:36 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,12 +3,14 @@
 /**
 *more_numbers - function that prints 10 times the numbers, 0 to 14
 *followed by a new line
+*Return: 0
 */
 
 void more_numbers(void)
 {
 int i;
 int n;
+
 
 for (i = 0; i <= 9; i++)
 {
