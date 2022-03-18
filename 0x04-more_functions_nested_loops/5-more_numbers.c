820d1f5bf7c8d537f9abc9d03ab742ac176c3e8e
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-18 15:49:02 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,11 +16,11 @@ int n;
 {
 for​ (n = 0​; n <= 14​; n++)
 {
-if​ (n > 9​)
+if (n > 9)
 {
-_putchar​('1');
+_putchar('1');
 }
-​_putchar((n % 10) + '0');
+_putchar((n % 10) + '0');
 }
 _putchar('\n');
 }
