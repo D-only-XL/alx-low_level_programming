66a54709f20665db70580721bf599c3c01c10908
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-25 23:33:25 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include main.h
+
+/**
+*_strncat - concentrates two strings
+*@dest: A pointer of a character that will be changed
+*@src: A pointer that a character that will also be changed
+@n: value
+Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+j = 0;
+while (j < n && src [j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dear[i] = '\0';
+return (dest);
+}
