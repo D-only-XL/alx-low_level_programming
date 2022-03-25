06944435b834df3355ce1635c5e16e69b19419b4
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-26 00:43:22 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,10 +2,10 @@
 
 /**
 *reverse_array - reverses the content of an array of integers
-**@a: an array of integers
-**@n: number of elements of the array
+*@a: an array of integers
+*@n: number of elements of the array
 *Return: returns void
-**/
+*/
 
 void reverse_array(int *a, int n);
 
