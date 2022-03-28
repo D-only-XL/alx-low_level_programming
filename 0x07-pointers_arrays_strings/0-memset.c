5bfd9f3a6c7234744feaec67fc835dcd2abba4b3
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-28 17:20:01 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#​include​ ​"​main.h"
+#include "main.h"
 
 /**
 *_memset - fills memory with constant byte
@@ -9,7 +9,7 @@
 *Return: the memory area of s
 */
 char *_memset(char *s, char b,
-unsigned int n);
+unsigned int n)
 {
 unsigned int i = 0;
 
