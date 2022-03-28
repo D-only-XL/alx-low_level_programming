8ce046e5cc054d8b67a0318c3fad0ded3487dca1
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-29 00:39:01 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /*​*
 *_strstr - locates a substring
 *@haystack: string
@@ -10,11 +11,11 @@ char *_strstr(char *haystack, char *needle)
 {
 int i;
 int j;
-for(i = 0; haystack[i] != '\0';)
+for (i = 0; haystack[i] != '\0';)
 {
-for(j = 0; needle[j] != '\0'; j++)
+for (j = 0; needle[j] != '\0'; j++)
 {
-if (needle[j] != haystack [i + j])
+if (needle[j] != haystack[i + j])
 {
 break;
 }
