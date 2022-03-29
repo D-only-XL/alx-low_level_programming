e73ccf4941d4d294c8a772a328a0805923260d0b
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-29 02:36:46 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
-*print_diagsums - prints the sum of the two
-diagonals of a square matrix
+*print_diagsums - prints the sum of the two diagonals
+ of a square matrix
 *
 *@a:pointer that contains the address of the beginning of the matrix
 *@size: size of the square matrix
 *
 *Return: nothing
 */
-void​ ​print_diagsums​(​int​ *a, ​int​ size)
+void print_diagsums(int *a, int size)
 
 {
 int i, j;
 int d1 = 0, d2 = 0;
 
-for (i = 0​; i < size * size; i = i + (size + 1​))
+for (i = 0​; i < size * size; i = i + (size + 1​))
 {
 d1 = d1 + a[i];
 }
