12f8f6e7279f7dfed01f1b7208142dd9de509076
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-29 17:55:31 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 
 /**
 *print_diagsums - prints the sum of the two diagonals
-of a square matrix
+ of a square matrix
 *
 *@a:pointer that contains the address of the beginning of the matrix
 *@size: size of the square matrix
