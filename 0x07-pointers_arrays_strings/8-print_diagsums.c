f45be8520b29a2ba3be139c366f05bc7788299df
D-only-XL <99492723+D-only-XL@users.noreply.github.com>
2022-03-29 18:11:46 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,13 +16,13 @@ void print_diagsums(int *a, int size)
 int i, j;
 int d1 = 0, d2 = 0;
 
-for (i = 0​; i < size * size; i = i + (size + 1​))
+for (i = 0; i < size * size; i = i + (size + 1))
 {
 d1 = d1 + a[i];
 }
-for (j = size - ​1​; j < size * size - ​1​; j = j + (size - 1))
+for (j = size - 1; j < size * size - 1; j = j + (size - 1))
 {
 d2 = d2 + a[j];
 }
-print("%d, ​%d\n", d1, d2);
+printf("%d, ​%d\n", d1, d2);
 }
